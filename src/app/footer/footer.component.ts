e942e4f15ae6f0d3ea412c0d1eba653586a5b7ef
Priyanshu Kumar <priyanshu.kumar@biz2credit.com>
2023-03-06 02:18:25 +0530

    used ngFor

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  
+  icons=[
+    {title:"facebook",icon:"fab fa-facebook",link:"https://www.facebook.com/"},
+    {title:"github",icon:"fab fa-github",link:"https://github.com/priyanshu025"},
+    {title:"linkedIn",icon:"fab fa-linkedin",link:"https://www.linkedin.com/in/priyanshu-kumar-3260551ab/"},
+    {title:"gmail",icon:"fab fa-google",link:"https://www.gmail.com/"}
+  ]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectsComponent implements OnInit {
 
   constructor() { }
-
+  projects=[
+    {title:"Iota Shopping",image:"assets/iotaShopping.png",content:"It is a 3D visualization augmented reality E-commerce Application where customers can choose objects and furniture based on their real spaces.",link:"https://github.com/priyanshu025/iotaShopping"},
+    {title:"NFT Market Place", image:"assets/nftMarketplace.webp",content:"It is a Blockchain project which creates a marketplace for selling and buying NFTs",link:"https://github.com/priyanshu025/NFT_MarketPlace"}
+  ]
   ngOnInit(): void {
   }
 
